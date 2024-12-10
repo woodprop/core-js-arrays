aeb2a8a9b359eeff75844af2a7071987c8276d74
Konstantin <woodprop@yandex.ru>
2024-12-11 00:08:50 +0300

    feat: add getElementByIndices solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -377,8 +377,13 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  let res = arr;
+  indices.map((el) => {
+    res = res[el];
+    return false;
+  });
+  return res;
 }
 
 /**
